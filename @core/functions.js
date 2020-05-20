efc61f16381cd92c1728011689258168c14f21eb
Peter Kimutai <peter.kimutai@digitalvision.co.ke>
2020-05-20 11:50:26 +0300

    Fix issues highlighted by codacy

--- @core/functions.js
+++ @core/functions.js
@@ -41,13 +41,13 @@ const formatDate = function(date){
 const ordinalSuffixOf = function (i) {
     var j = i % 10,
         k = i % 100;
-    if (j === 1 && k != 11) {
+    if (j === 1 && k !== 11) {
         return i + "st";
     }
-    if (j === 2 && k != 12) {
+    if (j === 2 && k !== 12) {
         return i + "nd";
     }
-    if (j === 3 && k != 13) {
+    if (j === 3 && k !== 13) {
         return i + "rd";
     }
     return i + "th";
