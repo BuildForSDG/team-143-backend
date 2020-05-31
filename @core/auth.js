5547192b62de19e2c498c51232c2b4e3266d3006
Peter Kimutai <peter.kimutai@digitalvision.co.ke>
2020-05-31 17:18:52 +0300

    Progress

--- @core/auth.js
+++ @core/auth.js
@@ -17,43 +17,43 @@ config.app.post("/auth/login",function(req,res){
     query.password = config.crypto.createHash('md5').update(req.body.password).digest('hex');
     models.user_model.findOne(query,function(err,user){
         if(err){
-            res.send(err);
+            res.status(500).send(err);
         }else{
             if(user){
                 if(user.accessToken){
-                    res.send(user);                
+                    res.status(200).send(user);                
                 }else{
                     models.user_model.findByIdAndUpdate(user._id,{ accessToken: functions.generateRandomString(50) },function(err,result){
                         if(err){
-                            res.send(err);
+                            res.status(500).send(err);
                         }else{
-                            res.send(user);
+                            res.status(200).send(user);
                         }
                     });
                 }
             }else{
-                res.send({});
+                res.status(401).send('The username/password is incorrect');
             }
         }
     });
 });
 
-config.app.post("/auth/get_user_by_token",function(req,res){ 
+config.app.post("/auth/getUserByAccessToken",function(req,res){ 
     if(req.body.accessToken){
-    	models.user_model.findOne({accessToken:req.body.accessToken},function(err,user){
+    	models.user_model.findOne({_id: config.ObjectID(req.body.userId), accessToken:req.body.accessToken},function(err,user){
     		if(err){
-    			res.send(err);
+    			res.status(500).send(err);
     		}else{
     			if(user){
     				models.user_model.findByIdAndUpdate(user._id,{ lastSeenOn: Date.now() },function(err,result){
     					if(err){
-    						res.send(err);
+    						res.status(500).send(err);
     					}else{
-    						res.send(user);
+    						res.status(200).send(user);
     					}
     				});
     			}else{
-    				res.send({});
+    				res.status(404).send('User not found');
     			}
     		}
     	});
